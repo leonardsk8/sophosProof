4deabb66520047c38f70763d34ab1ef244800a24
leonardo <leonard-sk8@hotmail.com>
2021-03-04 10:35:40 -0500

    Feature: init commit technical proof

---
+++ main/src/sophos/prueba/Sophos.java
@@ -0,0 +1,71 @@
+package sophos.prueba;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Sophos {
+
+    private static String[][] matriz;
+    private static int matrixSize;
+
+    public static void main(String[] args) {
+        getNumberByUser();
+
+        fillMatrixDash();
+
+        fillXMainDiagonal();
+
+        fillXInvertedDiagonal();
+
+        printMatrix();
+    }
+
+    private static void getNumberByUser() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What is the size? ");
+
+        try {
+            matrixSize = scanner.nextInt();
+            if (matrixSize == 0) {
+                System.out.print("ERROR!");
+                System.exit(0);
+            }
+        }catch (InputMismatchException e){
+            System.out.print("ERROR!");
+            System.exit(0);
+        }
+    }
+
+    private static void fillMatrixDash() {
+        matriz = new String[matrixSize][matrixSize];
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++) {
+                matriz[i][j] = "_";
+            }
+        }
+    }
+
+    private static void fillXMainDiagonal() {
+        for (int i = 0; i < matrixSize; i++) {
+            matriz[i][i] = "x";
+        }
+    }
+
+    private static void fillXInvertedDiagonal() {
+        int j=0;
+        for (int i = matrixSize - 1; i >= 0; i--) {
+            matriz[i][j] = "x";
+            j++;
+        }
+    }
+
+    private static void printMatrix() {
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++) {
+                System.out.print(matriz[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
